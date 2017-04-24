30c059de95a37c1cc1216d2d8e71be828cbdf812
Serg T <sergtsaeb@gmail.com>
2017-04-24 09:29:20 -0700

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 4/24/17.
+//  Copyright © 2017 Serg Tsogtbaatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
